85a5a6f4a8c407f28a84938a9352ffea3811bf35
codeup38 <53144836+codeup38@users.noreply.github.com>
2020-06-18 10:41:58 +0900

    codeup submit time crawler beta
    beta

---
+++ codeup 제출시간.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+user_agent = {'User-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
+
+req = requests.get('https://codeup.kr/status.php', headers = user_agent)
+html = req.text
+result = BeautifulSoup(html, "html.parser")
+
+submit = result.select (
+    'a'
+    )
+
+for i in range(len(submit)):
+    print(submit[i], '\n')
